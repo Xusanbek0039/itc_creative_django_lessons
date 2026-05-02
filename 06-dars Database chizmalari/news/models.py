a69dfe1359566cf026fc9e8292b5fde537f1c9fc
Husanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2026-05-02 15:00:24 +0500

    Update

--- 06-dars Database chizmalari/news/models.py
+++ 06-dars Database chizmalari/news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    file = models.FileField(upload_to='news_file/', blank=True, null=True)
+    likes = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    tags = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
