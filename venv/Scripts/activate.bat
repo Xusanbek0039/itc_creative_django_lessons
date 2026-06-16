9f33c549a0fcc73984fe6d9be000cf8d7bc5bc96
Husanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2026-06-16 13:50:46 +0500

    Update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\husanbek-coder.uz\Desktop\itc_creative_django_lessons\venv
+set VIRTUAL_ENV=C:\Users\itpar\OneDrive\Desktop\itc_creative_django_lessons\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
